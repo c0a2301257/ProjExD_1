6c3694f7128955271bdf79f32e8b6d977775d1be
ishida <c0a2301257@edu.teu.ac.jp>
2024-06-11 16:42:28 +0900

    上下左右の移動が可能に

--- pygame_image.py
+++ pygame_image.py
@@ -14,8 +14,13 @@ def main():
     bg2_img = pg.image.load("fig/pg_bg.jpg")
     koka_img = pg.transform.flip(koka_img,True,False)#画像反転
     bg2_img = pg.transform.flip(bg2_img,True,False)
+    koka_rect = koka_img.get_rect()#こうかとんrect抽出
+    koka_rect.center = 300,200
     tmr = 0
     x = 0
+
+    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -23,13 +28,23 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg2_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg2_img, [-x+6400, 0])
-        koka_rect = koka_img.get_rect()#こうかとんrect抽出
-        koka_rect.center = 300,200
+        screen.blit(bg2_img, [-x+4800, 0])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koka_rect.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            koka_rect.move_ip((0,+1))
+        elif key_lst[pg.K_LEFT]:
+            koka_rect.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            koka_rect.move_ip((+1,0))
+
         screen.blit(koka_img,koka_rect)#koka_imgをkoka_rectの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+    
 
 
 if __name__ == "__main__":
