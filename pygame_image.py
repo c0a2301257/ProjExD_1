20d64f479bf46eab0818cbce9aface4fb61a7904
ishida <c0a2301257@edu.teu.ac.jp>
2024-06-11 16:28:20 +0900

    背景画像のループを長く

--- pygame_image.py
+++ pygame_image.py
@@ -11,15 +11,17 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     koka_img = pg.image.load("fig/3.png")
+    bg2_img = pg.image.load("fig/pg_bg.jpg")
     koka_img = pg.transform.flip(koka_img,True,False)#画像反転
-
+    bg2_img = pg.transform.flip(bg2_img,True,False)
     tmr = 0
     x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%800
+        x = tmr%1600
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg2_img, [-x+1600, 0])
         koka_rect = koka_img.get_rect()#こうかとんrect抽出
         koka_rect.center = 300,200
         screen.blit(koka_img,koka_rect)#koka_imgをkoka_rectの設定に従って貼り付け
